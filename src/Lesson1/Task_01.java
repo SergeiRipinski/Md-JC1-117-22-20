c67899d102b5476b9227c77fa26e2e45cc7650ff
Sergei Ripinski <s.ripinski@yandex.by>
2022-09-11 21:52:38 +0300

    feat: add task 01-02

--- src/Lesson1/Task_01.java
+++
@@ -1,7 +0,0 @@
-package Lesson1;
-
-public class Task_01 {
-    public static void main(String[] args) {
-        System.out.println("Java: version 11.");
-    }
-}
--- src/Lesson1/Task_02.java
+++
@@ -1,7 +0,0 @@
-package Lesson1;
-
-public class Task_02 {
-    public static void main(String[] args) {
-        System.out.println("Java: version 11.\n" +"JVM: HotSpot.");
-    }
-}
