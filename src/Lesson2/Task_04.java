4e8d741df5c02b66264ab12cd60eca12a3ee8f9e
Sergei Ripinski <s.ripinski@yandex.by>
2022-09-11 19:56:40 +0300

    feat: add task 01-05

---
+++ src/Lesson2/Task_04.java
@@ -0,0 +1,14 @@
+package Lesson2;/*В переменной n хранится целое трехзначное число.
+  Создайте программу, вычисляющую и выводящую на экран сумму цифр числа n.
+*/
+import java.util.Scanner;
+
+public class Task_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите трехзначное целое число"); //просим пользователя ввести целое трехзначное число и записываем его в переменную n в 10 строке
+        int n = scanner.nextInt();
+
+        System.out.println("Сумма цифр числа " +n +" равна "+((n%10)+((n/10)%10)+((n/100)%10)));
+    }
+}
